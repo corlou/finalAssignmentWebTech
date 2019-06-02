1850fcaed40f06034f767f6fd8799555ca9d863e
Corina Pelichowski <corina@danitt.com>
2019-06-02 15:59:29 +1000

    addition of return to top arrow & fix map placement with z-index

--- script.js
+++ script.js
@@ -5,24 +5,18 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    const myButton = document.getElementById("myBtn");
+    const myButton = document.getElementById("back-top");
     // check if button exists before attempting to apply style
     if (myButton) {
       myButton.style.display = "block";
     }
   } else {
-    const myButton = document.getElementById("myBtn");
+    const myButton = document.getElementById("back-top");
     // check if button exists before attempting to apply style
     if (myButton) {
       myButton.style.display = "none";
     }
   }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
 }
 
 // make overlay disappear after 2 seconds
